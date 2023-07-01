8a2b1ddce9eac55ca46e47cdc6cd821945ad0e89
gmjishara <janith971231@gmail.com>
2023-07-01 22:18:04 +0530

    set the layout for search area and button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import "./App.css";
 import { useState } from "react";
 import SearchBar from "./SearchBar";
-import { Box } from "@mui/material";
+import { Box, Card } from "@mui/material";
 import TaskDisplayer from "./TaskDisplayer";
 
 function App() {
   const [data, setData] = useState([]);
 
   const mainStyle = {
-    wdith:"100%",
-    height:"100vh",
-    display:"flex",
-    flexDirection:"column",
+    wdith: "100%",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-  }
+    backgroundColor: "#3b5998",
+  };
 
   return (
     <Box style={mainStyle}>
-      <SearchBar data={data} setData={setData} />
-      <TaskDisplayer data={data}/>
+      <Card
+        sx={{
+          minWidth: 350,
+          minHeight: 150,
+          borderRadius: "8px",
+          position: "relative",
+          padding:"28px 20px"
+        }}
+      >
+        <SearchBar data={data} setData={setData} />
+        <TaskDisplayer data={data} />
+      </Card>
     </Box>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,31 +1,44 @@
 import React, { useState } from "react";
-import { TextField } from "@mui/material";
-import {Button, Grid} from "@mui/material";
+import { Box, TextField } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 
-export default function SearchBar({data,setData}) {
+export default function SearchBar({ data, setData }) {
   const [value, setValue] = useState("");
 
-  const addList = ()=>{
-    setData([...data,value])
-  }
+  const addList = () => {
+    setData([...data, value]);
+  };
+
+  // const searchStyle = {
+  //   position: "absolute",
+  //   top: "28px",
+  //   left: "20px",
+  // };
 
   return (
-    <div>
+    <Box>
       <Grid container spacing={2} direction="row">
-        <Grid item>
+        <Grid item xs={12} sm={8} md={8} lg={8} xl={9}>
           <TextField
             id="outlined-basic"
             label="What is the task today?"
             variant="outlined"
+            size="small"
             onChange={(event) => setValue(event.target.value)}
+            style={{ width: "100%" }}
           />
         </Grid>
-        <Grid item>
-          <Button variant="outlined" color="info" onClick={addList}>
+        <Grid item xs={12} sm={4} md={4} lg={4} xl={3}>
+          <Button
+            variant="contained"
+            color="info"
+            onClick={addList}
+            style={{ width: "100%" }}
+          >
             Add
           </Button>
         </Grid>
       </Grid>
-    </div>
+    </Box>
   );
 }
