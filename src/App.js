7b04780c199390cc1a71737cccc7966be73b50eb
gmjishara <janith971231@gmail.com>
2023-07-02 02:33:45 +0530

    set heading to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import SearchBar from "./SearchBar";
-import { Box, Card } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 import TaskDisplayer from "./TaskDisplayer";
 
 function App() {
@@ -13,22 +13,29 @@ function App() {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center",
     backgroundColor: "#3b5998",
+  };
+
+  const headingStyle = {
+    margin: "0 0 30px 120px",
+    fontWeight: 700,
+    fontSize: "25px",
   };
 
   return (
     <Box style={mainStyle}>
       <Card
         sx={{
-          minWidth:350,
+          minWidth: 350,
           maxWidth: 350,
           maxHeight: 500,
           borderRadius: "8px",
-          padding: "28px 20px ",
-          overflowY:data.length>7? "scroll":"hidden",
+          marginTop: "50px",
+          padding: data.length > 0 ? "28px 20px" : "28px 20px 0 20px",
+          overflowY: data.length > 7 ? "scroll" : "hidden",
         }}
       >
+        <Typography style={headingStyle}>Todo List</Typography>
         <SearchBar data={data} setData={setData} />
         <TaskDisplayer data={data} setData={setData} />
       </Card>
