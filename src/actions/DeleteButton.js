ec0b87002b5cad1b15f3308e5a124df420dfa6e2
gmjishara <janith971231@gmail.com>
2023-07-03 22:45:29 +0530

    added a sweetalert when deleting an item

--- src/actions/DeleteButton.js
+++ src/actions/DeleteButton.js
@@ -1,10 +1,24 @@
 import { Button } from "@mui/material";
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Swal from "sweetalert2";
 
 export default function DeleteButton({ place, data, setData }) {
   const deleteItem = () => {
-    setData(data.filter((element) => element !== data[place]));
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setData(data.filter((element) => element !== data[place]));
+        Swal.fire("Deleted!", "Your task has been deleted.", "success");
+      }
+    });
   };
   return (
     <div>
