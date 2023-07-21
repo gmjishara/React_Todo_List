b75697d2e15df8fa32ebbabda6a15de2f7b5d4db
gmjishara <janith971231@gmail.com>
2023-07-21 17:38:17 +0530

    set gap between tasks

--- src/TaskDisplayer.js
+++ src/TaskDisplayer.js
@@ -9,19 +9,21 @@ export default function TaskDisplayer({ data, setData }) {
     fontWeight: 500,
     fontSize: "23px",
   };
+
+  const gridStyles = {
+    display: "flex",
+    flexDirection: "column",
+    gap: 5,
+  };
   return (
     <CardComponent minWidth={350} maxWidth={350} maxHeight="auto">
       {data.length === 0 ? (
         <Typography style={headingStyle}>No Data</Typography>
       ) : (
-        <Grid container spacing={1} direction={"column"}>
+        <Grid container spacing={1} direction={"column"} style={gridStyles}>
           {data.map((item, index) => (
             <Grid item>
-              <ListItem
-                place={index}
-                data={data}
-                setData={setData}
-              >
+              <ListItem place={index} data={data} setData={setData}>
                 {item}
               </ListItem>
             </Grid>
